7ff755a81b59f9cb47137d5e018bcbee3c7473f4
Priyanshu Sharma <prynsh1112@gmail.com>
2023-04-28 16:30:33 -0700

    added logging for wandb usage

--- finetune.py
+++ finetune.py
@@ -66,6 +66,7 @@ def train(
     use_wandb = len(wandb_project) > 0 or (
         "WANDB_PROJECT" in os.environ and len(os.environ["WANDB_PROJECT"]) > 0
     )
+    print("Wandb - {}".format(use_wandb))
     # Only overwrite environ if wandb param passed
     if len(wandb_project) > 0:
         os.environ["WANDB_PROJECT"] = wandb_project
