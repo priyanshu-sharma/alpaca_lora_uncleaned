4f90f44867803a05e2c92f1c721b965d8a2ed104
Priyanshu Sharma <prynsh1112@gmail.com>
2023-05-06 17:07:07 -0700

    fix issue 203

--- finetune.py
+++ finetune.py
@@ -153,29 +153,30 @@ def train(
         return result
 
     def generate_and_tokenize_prompt(data_point):
-        full_prompt = prompter.generate_prompt(
-            data_point["instruction"],
-            data_point["input"],
-            data_point["output"],
-        )
-        tokenized_full_prompt = tokenize(full_prompt)
-        if not train_on_inputs:
-            user_prompt = prompter.generate_prompt(
-                data_point["instruction"], data_point["input"]
+        with torch.autocast("cuda"):
+            full_prompt = prompter.generate_prompt(
+                data_point["instruction"],
+                data_point["input"],
+                data_point["output"],
             )
-            tokenized_user_prompt = tokenize(
-                user_prompt, add_eos_token=add_eos_token
-            )
-            user_prompt_len = len(tokenized_user_prompt["input_ids"])
+            tokenized_full_prompt = tokenize(full_prompt)
+            if not train_on_inputs:
+                user_prompt = prompter.generate_prompt(
+                    data_point["instruction"], data_point["input"]
+                )
+                tokenized_user_prompt = tokenize(
+                    user_prompt, add_eos_token=add_eos_token
+                )
+                user_prompt_len = len(tokenized_user_prompt["input_ids"])
 
-            if add_eos_token:
-                user_prompt_len -= 1
+                if add_eos_token:
+                    user_prompt_len -= 1
 
-            tokenized_full_prompt["labels"] = [
-                -100
-            ] * user_prompt_len + tokenized_full_prompt["labels"][
-                user_prompt_len:
-            ]  # could be sped up, probably
+                tokenized_full_prompt["labels"] = [
+                    -100
+                ] * user_prompt_len + tokenized_full_prompt["labels"][
+                    user_prompt_len:
+                ]  # could be sped up, probably
         return tokenized_full_prompt
 
     model = prepare_model_for_int8_training(model)
